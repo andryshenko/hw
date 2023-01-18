06bf13e61e9b1abe95647ec1a8c73ee1f78ca969
olegandruschenko <andryshenko@gmail.com>
2023-01-18 10:09:25 +0300

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+System.Console.Write("Введите любое число --> ");
+int a=Convert.ToInt32(Console.ReadLine());
+int (n - 1);
+
+if (a % 2 == 0)
+{
+    System.Console.WriteLine();    
+}
