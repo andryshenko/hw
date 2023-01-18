9015061988fbed6a958aad2cd7f19fc84fddf598
olegandruschenko <andryshenko@gmail.com>
2023-01-18 09:07:26 +0300

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+System.Console.Write("Введите первое число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int b=Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
