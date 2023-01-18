186eea0a1a89c3768ef30c6b9c3140ef107024f2
olegandruschenko <andryshenko@gmail.com>
2023-01-18 09:14:26 +0300

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -5,8 +5,13 @@ int a=Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите второе число: ");
 int b=Convert.ToInt32(Console.ReadLine());
 int max = a;
+int min = b;
 if (a > max) max = a;
+min = b;
 if (b > max) max = b;
+min = a;
 
 Console.Write("max = ");
 Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
