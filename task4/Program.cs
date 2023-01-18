f0a63f7d024e77f579521af78b073419c2144c5d
olegandruschenko <andryshenko@gmail.com>
2023-01-18 09:17:01 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+System.Console.Write("Введите первое число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int b=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третье число: ");
+int c=Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
