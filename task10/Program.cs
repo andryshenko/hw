5c74df6e675eef60071c6cd488cb84474e245fea
olegandruschenko <andryshenko@gmail.com>
2023-01-24 00:14:01 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите трехзначное число.\nВыход из программы командой Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
