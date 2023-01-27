7a93fd9ae5b8a0e14247d535bcb01fd6be9a0322
olegandruschenko <andryshenko@gmail.com>
2023-01-27 20:16:01 +0300

    task 13

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Является ли этот день недели - Выходным?");
+﻿Console.Clear();
+Console.WriteLine("Является ли этот день недели - Выходным?");
 Console.WriteLine("Введите номер дня недели");
 int number = int.Parse(Console.ReadLine()!);
 
@@ -9,6 +10,24 @@ if(day >= 1 && day <= 5)
         Console.WriteLine("нет");
     }
 
-    else Console.WriteLine("да");
-      
+    else Console.WriteLine("да");      
 }
+
+// int a=7;
+// switch (a)
+// {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         System.Console.WriteLine("Рабочий день");
+//         break;
+//     case 6:
+//     case 7:
+//         System.Console.WriteLine("Выходной");
+//         break;
+//     default:
+//         System.Console.WriteLine("Нет такого дня недели");
+//         break;
+// }
