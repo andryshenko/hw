cd63c30d13d54616919df1257d34cba9a187c5db
olegandruschenko <andryshenko@gmail.com>
2023-01-27 20:16:27 +0300

    task 13

---
+++ task13/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+// Вариант 1
+// Console.WriteLine("Введите любое число: ");
+
+// string? str = Console.ReadLine();
+// if (str == null) return;
+
+// System.Console.WriteLine(str[2]);
+
+// Вариант 2
+int n=456879;
+
+while (n>999)
+{
+    n=n/10;
+    System.Console.WriteLine(n);
+}
+System.Console.WriteLine(n%10);
+
+// Вариант 3
+// Console.Write("Введите любое число: ");
+// int a=Convert.ToInt32(Console.ReadLine());
+// int b=a.ToString().Length;
+// int divider=Convert.ToInt32(Math.Pow(10, b-3));
+
+// if (b<3)
+// {
+//     Console.WriteLine("Третья цифра отсутствует");
+// }
+// else
+// {
+//     Console.WriteLine((a/divider)%10);
+// }
