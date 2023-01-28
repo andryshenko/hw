180836267786a0bab73e9802d9c5df84f8d8f241
olegandruschenko <andryshenko@gmail.com>
2023-01-28 09:44:38 +0300

    task23

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// Вариант 1
+// for (int i=1; i<=n; i++)
+// {
+//     System.Console.Write($" {i*i*i} ");
+// }
+
+// Вариант 2
+int stepen = 3;
+Console.WriteLine("n ^ 3 = " + Math.Pow(n, stepen));
