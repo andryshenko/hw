db1ef1c6f76cf07375e0d9bc94bf5e96e2eee213
olegandruschenko <andryshenko@gmail.com>
2023-01-28 10:23:12 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите **ПЯТИЗНАЧНОЕ** число: ");
+string number = Console.ReadLine();
+int lenght = number.Length;
+
+if (lenght == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("ОШИБКА - введено не пятизначное число");
+}
