0f69083fab27b720be21e4cdf2baaecb0c38f950
olegandruschenko <andryshenko@gmail.com>
2023-01-29 10:06:35 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на входе принимает число 
+// и выдает сумму цифр в числе.   (452 = 4+5+2 = 11)
+Console.Clear();
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+while (a != 0)
+{
+    result += a % 10;
+    a /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе = {result}");
