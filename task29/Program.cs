6baf5f5608c02ae4c077e29907eee6851cbc6de8
olegandruschenko <andryshenko@gmail.com>
2023-01-29 10:33:08 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая задаёт массив 
+//из 8 элементов и выводит их на экран.
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+
+for (int i = 0; i < number; i++)
+{
+    array[i] = new Random().Next(number + 1);
+    Console.Write(array[i] + ", ");
+}
+
+System.Console.WriteLine();
+
+// int index = 0;
+
+// if (index <= 8)
+// {
+//     Console.Write("ОШИБКА - Массив более 8 символов!");
+// }
+// Хочу ограничить ввод массива более 8 символов, но что-то видимо
